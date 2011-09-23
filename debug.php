800ea84f676a8e53dee4d462b0f74d323f4ee0af
Nick Barrett <nick@fanaticaldev.com>
2011-09-23 15:58:56 +0100

    more style fixes (z-index again!)

--- debug.php
+++ debug.php
@@ -25,14 +25,14 @@
 			global $c_config;
 			if( $c_config['debug'] ):
 				echo '<!--core debug-->
-		<a title="View debug" href="#debug" onclick="if(document.getElementById(\'c_debug\').style.display == \'block\' ) { document.getElementById(\'c_debug\').style.display = \'none\'; history.go( -1 ); } else { document.getElementById(\'c_debug\').style.display=\'block\'; }" style="position:fixed;top:0;right:0;color:#333333;text-shadow:none;background:#F7F7F7;z-index:99999999999999;font-family:Arial;font-size:12px;text-decoration:none;padding:5px;border:1px solid #D7D7D7;border-width:0 0 1px 1px;">Debug</a>
-		<div id="c_debug" style="position:fixed;top:0;left:0;width:98%;height:98%;background:#EEEEEE;color:#1A1B1B;font-family:Arial;text-shadow:none;font-size:13px;line-height:20px;padding:1%;z-index:99999999999998;display:none;overflow:auto;"><h2>Core Debug</h2>';
+		<div id="c_debug" style="position:fixed;top:0;left:0;width:98%;height:98%;background:#EEEEEE;color:#1A1B1B;font-family:Arial;text-shadow:none;font-size:13px;line-height:20px;padding:1%;z-index:9999999999;display:none;overflow:auto;"><h2>Core Debug</h2>';
 				foreach( $this->debugs as $k => $v ):
 					if( in_array( $k, $ignore ) ) continue;
 					echo '<strong>' . $v[2] . ':</strong> ' . $v[0] . ' <small>+' . round( ( $v[1] - $this->start ) * 1000, 5 ) . 'ms</small><br />';
 				endforeach;
 				echo '<br />End  Debug<small> +' . round( ( microtime( true ) - $this->start ) * 1000, 5 ) . 'ms</small>';
 				echo '</div><!--end c_debug--><script type="text/javascript">if( String( window.location ).search( \'#debug\' ) > -1 ) { document.getElementById( \'c_debug\' ).style.display = \'block\'; }</script>
+		<a title="View debug" href="#debug" onclick="if(document.getElementById(\'c_debug\').style.display == \'block\' ) { document.getElementById(\'c_debug\').style.display = \'none\'; history.go( -1 ); } else { document.getElementById(\'c_debug\').style.display=\'block\'; }" style="position:fixed;top:0;right:0;color:#333333;text-shadow:none;background:#F7F7F7;z-index:9999999999;font-family:Arial;font-size:12px;text-decoration:none;padding:5px;border:1px solid #D7D7D7;border-width:0 0 1px 1px;">Debug</a>
 		<!--/core debug-->';
 			endif;
 		}
