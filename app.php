1691618041998e3f440034b6fececfa9fb66746d
Filipe Dobreira <dobreira@gmail.com>
2011-09-23 03:59:50 +0100

    Implemented crude autoloader

--- app.php
+++ app.php
@@ -4,18 +4,43 @@
 	class c_app {
 		private $app_dir;
 		private $debug;
+		private $autoload_map;
 		
 		public function __construct( $app_dir = 'app/' ) {
-			global $c_debug;
+			global $c_debug, $c_config;
+
 			$this->app_dir = $app_dir;
 			//debug
 			$this->debug = $c_debug;
 			$this->debug->add( 'c_app loaded' );
+
+			
+			if(isset($c_config['autoload'])):
+				spl_autoload_register(array($this, 'autoload'), true, true);
+				if(isset($c_config['autoload_map']))
+					$this->autoload_map = $c_config['autoload_map'];
+			endif;
 		}
 		
 		public function load( $file ) {
 			global $c_config;
 			if( !include( $this->app_dir . $file . '.php' ) ) $this->debug->add( 'no load file found: ' . $file );
+		}
+
+		public function autoload($class)
+		{
+			global $c_config;
+			if(isset($this->autoload_map[$class]))
+				$path = $this->autoload_map[$class];
+			else
+				$path =   $c_debug['core_dir'] . DIRECTORY_SEPARATOR
+						. str_replace('_', DIRECTORY_SEPARATOR, $class) . '.php';
+				
+		
+			if(!include $path)
+				$this->debug->add("Failed to load $class @ '$path'");
+			else
+				$this->debug->add("$class autoloaded from '$path'");	
 		}
 	}
 ?>
--- core.php
+++ core.php
@@ -15,24 +15,43 @@
 		'debug' => true,
 		'core_dir' => __DIR__,
 		'core_ver' => C_CORE_VER,
+
+		// the autoloader only kicks in after c_app::__construct,
+		// and only if this configuration param is set to true.
+		'autoload' => true,
+
+		// map classes with names that do not correspond to their
+		// actual locations to an absolute path:
+		// NOTE: underscores are replaced with DIRECTORY_SEPARATOR,
+		// by default. Given the original naming scheme, a class
+		// like c_app would point to c/app.php, hence the presence
+		// of every core class in this list.
+		'autoload_map' => array(
+			'LightOpenID' => 'lib/LightOpenID.php',
+			'c_app' 	  => 'app.php',
+			'c_db' 		  => 'database.php',
+			'c_template'  => 'template.php',
+			'c_debug'     => 'debug.php',
+			'c_session'   => 'session.php',
+			'c_user'	  => 'user.php'
+		)
 	);
 	$c_debug->add( 'config loaded' );
 	
 	//load other files
 	$files = array(
-		'database',
-		'openid.lib',
-		'user',
-		'template',
 		'message',
 		'time',
-		'session',
 		'app'
 	);
+
 	foreach( $files as $file ):
 		if( !include( $c_config['core_dir'] . '/' . $file . '.php' ) ) $c_debug->add( 'Core file not found/loaded: ' . $file );
 	endforeach;
 	
 	//send some debug
 	$c_debug->add( 'core is loaded' );
+	new c_app;
+	new LightOpenID;
+	$c_debug->display();
 ?>
