60cca9341fe1fb3e4d29057fe0912c384303d58b
Nick Barrett <nick@fanaticaldev.com>
2012-02-21 15:16:05 +0000

    remove pointless config vars

--- core.php
+++ core.php
@@ -3,16 +3,12 @@
 	define( 'C_CORE_VER', '1.3.0' );
 
 	//start debug
-	if( !file_exists( __DIR__ . '/debug.php' ) ) die( 'Core fatal error: no debug.php' );
 	require( __DIR__ . '/debug.php' ); //important
 
 	//fd config, auto-generated array of useful shit
 	$c_config = array(
 		'root' => ( !empty( $_SERVER['HTTPS'] ) ? 'https' : 'http' ) . '://' . $_SERVER['HTTP_HOST'] . rtrim( dirname( $_SERVER['PHP_SELF'] ), '/' ), //base for all index.php routing-based apps
-		'dir' => str_replace( ' ', '%20', rtrim( dirname( $_SERVER['PHP_SELF'] ), '/' ) ),
 		'base' => ( !empty( $_SERVER['HTTPS'] ) ? 'https' : 'http' ) . '://' . $_SERVER['HTTP_HOST'],
-		'lang' => 'en',
-		'debug' => true,
 		'core_dir' => __DIR__,
 		'core_ver' => C_CORE_VER,
 		'autoload_map' => array(
